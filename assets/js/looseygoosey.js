6342d91af0b7f84311848817d48023cb847278b2
Michael Hwang <terranmoccasin@gmail.com>
2015-02-21 16:44:46 -0500

    Adding formatting for codeblocks and images

---
+++ assets/js/looseygoosey.js
@@ -0,0 +1,30 @@
+var LooseGoosey = LooseGoosey || {};
+
+LooseGoosey.constants = {
+  LINE_HEIGHT: 24
+};
+
+LooseGoosey.adjustImages = function() {
+  $.each($('.post img'), function(index, img) {
+    $(img).on('load', function() {
+      var height = $(img).height();
+      var totalPadding = Math.floor(height / LooseGoosey.constants.LINE_HEIGHT);
+      var topPadding = Math.floor(totalPadding / 2);
+      var botPadding = totalPadding - topPadding;
+      $(img).parent().css({
+        'margin': 0,
+        'padding-top': topPadding,
+        'padding-bottom': botPadding,
+        'padding-left': 0,
+        'padding-right': 0,
+        'height': height,
+        'margin-bottom': LooseGoosey.constants.LINE_HEIGHT
+      });
+      console.log($(img).height());
+    });
+  });
+};
+
+$(document).ready(function() {
+  LooseGoosey.adjustImages();
+});
