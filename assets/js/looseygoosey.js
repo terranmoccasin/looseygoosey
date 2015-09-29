e9efb2a0d6934d4abb0535024a273c1a04614d5b
Michael Hwang <mqhwang@google.com>
2015-09-29 18:37:44 -0400

    #18 Adding syntax highlighting

--- assets/js/looseygoosey.js
+++ assets/js/looseygoosey.js
@@ -4,28 +4,44 @@ LooseyGoosey.constants = {
   LINE_HEIGHT: 24
 };
 
+LooseyGoosey.adjustTag = function(tag) {
+  var height = $(tag).outerHeight();
+  var totalPadding = (Math.floor(height / LooseyGoosey.constants.LINE_HEIGHT) + 1)
+      * LooseyGoosey.constants.LINE_HEIGHT - height;
+  var topPadding = Math.floor(totalPadding / 2);
+  var botPadding = totalPadding - topPadding + 6;
+  // TODO(mqhwang): Make some kind of string replace method to pull this into a div template.
+  $(tag).before(
+    '<div class="spacer" style="margin-bottom: -6px; height:' + topPadding + 'px;"></div>');
+  $(tag).after(
+    '<div class="spacer" style="height:' + botPadding + 'px;"></div>');
+  $(tag).css({
+    'display': 'block'
+    // 'border': '1px solid #000',
+  });
+};
+
 LooseyGoosey.adjustTags = function($tags) {
-  $.each($tags, function(index, img) {
-    $(img).on('load', function() {
-      var height = $(img).height();
-      var totalPadding = (Math.floor(height / LooseyGoosey.constants.LINE_HEIGHT) + 1)
-          * LooseyGoosey.constants.LINE_HEIGHT - height;
-      var topPadding = Math.floor(totalPadding / 2);
-      var botPadding = totalPadding - topPadding + 6;
-      // TODO(mqhwang): Make some kind of string replace method to pull this into a div template.
-      $(img).before(
-        '<div class="img-spacer" style="margin-bottom: -6px; height:' + topPadding + 'px;"></div>');
-      $(img).after(
-        '<div class="img-spacer" style="height:' + botPadding + 'px;"></div>');
-      $(img).css({
-        'display': 'block',
-        // 'border': '1px solid #000',
-      });
+  $.each($tags, function(index, tag) {
+    LooseyGoosey.adjustTag(tag);
+  });
+};
+
+LooseyGoosey.adjustTagsAsync = function($tags) {
+  $.each($tags, function(index, tag) {
+    $(tag).load(function() {
+      LooseyGoosey.adjustTag(tag);
     });
   });
 };
 
+if (Prism) {
+  Prism.hooks.add('complete', function(env) {
+    LooseyGoosey.adjustTag(env.element.parentElement);
+  });
+}
+
 $(document).ready(function() {
-  LooseyGoosey.adjustTags($('.post img'));
-  LooseyGoosey.adjustTags($('.post iframe'))
+  LooseyGoosey.adjustTagsAsync($('.post img'));
+  LooseyGoosey.adjustTagsAsync($('.post iframe'));
 });
